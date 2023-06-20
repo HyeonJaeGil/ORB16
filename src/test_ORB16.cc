8ad6db5f226eb3b4dbf45816f018fd89efc056ac
HyeonJaeGil <now9728@gmail.com>
2023-06-20 20:27:14 +0900

    update test code for both 16bit and 8bit

--- src/test_ORB16.cc
+++ src/test_ORB16.cc
@@ -4,53 +4,80 @@
 
 using namespace cv;
 
-int main(int argc, char **argv) {
+void resizeKepoints(std::vector<KeyPoint> &keypoints, float scale) {
+  for (auto &kp : keypoints) {
+    kp.pt.x *= scale;
+    kp.pt.y *= scale;
+  }
+}
 
-  // // load image from assets
-  // Mat img = imread("../assets/lena.png", IMREAD_GRAYSCALE);
-  // cv::imshow("lena", img);
-  // cv::waitKey(0);
+int main(int argc, char **argv) {
 
   // load 16bit image from assets
   Mat img = imread("../assets/tir.png", IMREAD_UNCHANGED);
-  // cv::imshow("tir", img);
-  // cv::waitKey(0);
   Mat img8 = img.clone();
+
   // normalize img with min max value
   double min, max;
   cv::minMaxLoc(img, &min, &max);
   img.convertTo(img8, CV_8UC1, 255.0 / (max - min), -min * 255.0 / (max - min));
 
+  // resize to half
+  cv::Mat img_half, img8_half;
+  cv::resize(img, img_half, cv::Size(), 0.5, 0.5, cv::INTER_NEAREST);
+  cv::resize(img8, img8_half, cv::Size(), 0.5, 0.5, cv::INTER_NEAREST);
 
-  // test with fast detector
-  auto fast = FastFeatureDetector16::create(200, true, FastFeatureDetector16::TYPE_9_16);
-  std::vector<KeyPoint> keypoints_fast;
-  fast->detect(img, keypoints_fast);
-  std::cout << "keypoints_fast size: " << keypoints_fast.size() << std::endl;
+  ////////////////////////////////////////////////////////////////////////////
 
-  // draw keypoints
-  Mat img_keypoints_fast;
-  drawKeypoints(img8, keypoints_fast, img_keypoints_fast);
-  imshow("Keypoints_fast", img_keypoints_fast);
-  waitKey(0);
-
-
-  auto orb = ORB16::create(500, 1.2f, 8, 31, 0, 2, ORB16::HARRIS_SCORE, 31, 20);
-
-  // detect keypoints
-  std::vector<KeyPoint> keypoints;
-  orb->detect(img8, keypoints);
+  // test with fast detector 16bit, half size
+  auto fast =
+      FastFeatureDetector16::create(30, true, FastFeatureDetector16::TYPE_9_16);
+  std::vector<KeyPoint> keypoints_16;
+  fast->detect(img_half, keypoints_16);
+  resizeKepoints(keypoints_16, 2.0); // half size compensation
+  std::cout << "keypoints_16 size: " << keypoints_16.size() << std::endl;
 
   // draw keypoints
-  Mat img_keypoints;
-  drawKeypoints(img8, keypoints, img_keypoints);
-  imshow("Keypoints", img_keypoints);
+  Mat img_keypoints_16;
+  drawKeypoints(img8, keypoints_16, img_keypoints_16);
+  imshow("keypoints_16 (half)", img_keypoints_16);
   waitKey(0);
 
-  // compute descriptors
-  Mat descriptors;
-  orb->compute(img, keypoints, descriptors);
-  std::cout << "descriptor size: " << descriptors.size() << std::endl;
+  ////////////////////////////////////////////////////////////////////////////
+
+  // test with fast detector 8bit, half size
+  auto orb = ORB16::create(500, 1.2f, 8, 31, 0, 2, ORB16::HARRIS_SCORE, 31, 7);
+  std::vector<KeyPoint> keypoints_8_half;
+  orb->detect(img8_half, keypoints_8_half);
+  resizeKepoints(keypoints_8_half, 2.0); // half size compensation
+  std::cout << "keypoints_8_half size: " << keypoints_8_half.size()
+            << std::endl;
+
+  // draw keypoints
+  Mat img_keypoints_8_half;
+  drawKeypoints(img8, keypoints_8_half, img_keypoints_8_half);
+  imshow("keypoints_8 (half)", img_keypoints_8_half);
+  waitKey(0);
+
+  ////////////////////////////////////////////////////////////////////////////
+
+  // test with fast detector 8bit (ORB default)
+  std::vector<KeyPoint> keypoints_8;
+  orb->detect(img8, keypoints_8);
+  std::cout << "keypoints_8 size: " << keypoints_8.size() << std::endl;
+
+  // draw keypoints
+  Mat img_keypoints_8;
+  drawKeypoints(img8, keypoints_8, img_keypoints_8);
+  imshow("keypoints_8 (original)", img_keypoints_8);
+  waitKey(0);
+
+  // // compute ORB descriptors
+  // Mat descriptors;
+  // orb->compute(img, keypoints_16, descriptors);
+  // std::cout << "descriptor size (16bit): " << descriptors.size() <<
+  // std::endl; orb->compute(img8, keypoints_8, descriptors); std::cout <<
+  // "descriptor size (8bit): " << descriptors.size() << std::endl;
 
   return 0;
 }
